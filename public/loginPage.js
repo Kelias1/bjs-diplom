f5b6292c5d82b512c003b40e3011f13c493186fe
Elias <kel20@inbox.ru>
2023-06-26 11:34:31 +0300

    Поправка

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,3 +1,23 @@
+// 'use strict'
+
+// const userForm = new UserForm();
+
+// userForm.loginFormCallback = (data) => {
+// 	ApiConnector.login(data, (response) => {
+// 		if (response.success) {
+// 			location.reload();
+// 		} else userForm.setLoginErrorMessage(response.error);
+// 	});
+// }
+
+// userForm.registerFormCallback = (data) => {
+// 	ApiConnector.register(data, (response) => {
+// 		if (response.success) {
+// 			location.reload();
+// 		} else userForm.setRegisterErrorMessage(response.error);
+// 	});
+// }
+
 'use strict'
 
 const userForm = new UserForm();
@@ -6,7 +26,9 @@ userForm.loginFormCallback = (data) => {
 	ApiConnector.login(data, (response) => {
 		if (response.success) {
 			location.reload();
-		} else userForm.setLoginErrorMessage(response.error);
+		} else {
+			userForm.setLoginErrorMessage(response.error);
+		}
 	});
 }
 
@@ -14,6 +36,8 @@ userForm.registerFormCallback = (data) => {
 	ApiConnector.register(data, (response) => {
 		if (response.success) {
 			location.reload();
-		} else userForm.setRegisterErrorMessage(response.error);
+		} else {
+			userForm.setRegisterErrorMessage(response.error);
+		}
 	});
 }
